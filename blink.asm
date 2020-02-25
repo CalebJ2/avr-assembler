90a3106dca685db108db704e2d8a359a9e7d5385
CalebJ2 <calebinvents@gmail.com>
2020-02-25 15:04:21 -0800

    Fix clr instruction and speed up blink program

--- blink.asm
+++ blink.asm
@@ -9,17 +9,21 @@
 .def temp = r20
 
     ldi one, 1 ; initialize our "one" register
+    ldi temp, 0 ; make sure zero register is 0
+    mov zero, temp
     ldi temp, ledMask
     out DDRB, temp   ; Set led pin data direction to out
+    out PortB, temp  ; Set led pin to high
 Increment:
     add numLow, one ; increment lower by one
     adc numMid, zero ; add zero + carry bit to mid
     adc numUpper, zero ; same for upper
-    brcs TurnOn ; branch if upper carried/overflowed
+    cpi numUpper, 0b00011111 ; half of full register
+    breq TurnOn ; branch if numUpper is Same or Higher than value in previous instruction
     rjmp Increment ; else increment again
 TurnOn:
     ldi temp, ledMask
-    out PortB, temp  ; Set pin to high or low
+    out PortB, temp  ; Set pin to high
     clr numLow
     clr numMid
     clr numUpper
@@ -27,9 +31,13 @@ Increment2:
     add numLow, one ; increment lower by one
     adc numMid, zero ; add zero + carry bit to mid
     adc numUpper, zero ; same for upper
-    brcs TurnOff ; branch if upper carried/overflowed
+    cpi numUpper, 0b00011111 ; half of full register
+    breq TurnOff ; branch if numUpper is Same or Higher than value in previous instruction
     rjmp Increment2 ; else increment again
 TurnOff:
     mov temp, zero
-    out PortB, temp  ; Set pin to high or low
+    out PortB, temp  ; Set pin to low
+    clr numLow
+    clr numMid
+    clr numUpper
     rjmp Increment   ; start incrementing again
